bd25bf2fe9d11123ca31f7ef554e326f2eb8e950
Mayar Elmaghrabi <46622715+MAYAR1996@users.noreply.github.com>
2019-05-31 14:52:34 +0200

    Update testCase.cs

--- testCase.cs
+++ testCase.cs
@@ -1,1 +1,10 @@
-
+[Test]
+public void AConstructedErrorContainsTheFaultingItemName()
+{
+    IErrorBuilder builder = new ErrorBuilder();
+    string faultingItem = "Faulting Item";
+ 
+    string errorMessage = builder.BuildError(faultingItem);
+ 
+    StringAssert.Contains(faultingItem, errorMessage);
+}
